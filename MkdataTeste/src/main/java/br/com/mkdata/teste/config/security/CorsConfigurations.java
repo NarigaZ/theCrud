a4d064892503b11ab58442030c750a6d768ba2bb
Felipe Rezende <felipe.rezende@acttive.com.br>
2021-06-13 20:11:25 -0300

    Crud Default Version 1.0.5

--- MkdataTeste/src/main/java/br/com/mkdata/teste/config/security/CorsConfigurations.java
+++ MkdataTeste/src/main/java/br/com/mkdata/teste/config/security/CorsConfigurations.java
@@ -1,14 +1,14 @@
-//package br.com.mkdata.teste.config.security;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class CorsConfigurations implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**").allowedMethods("*");
-//    }
-//}
+package br.com.mkdata.teste.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfigurations implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
+}
